d686ebc08149b24c4a1c774420852cf99b8ce48d
OUSSAMA-GD <oussama_wassif@um5.ac.ma>
2025-06-17 17:26:56 +0100

    fix: rendre render-build.sh exécutable

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+# Mise à jour de pip
 pip install --upgrade pip
-pip install wheel
+
+# Installation des outils nécessaires pour les builds
+pip install wheel setuptools
+
+# Installation des dépendances du projet
 pip install -r requirements.txt
